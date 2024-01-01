43d894d29009949f8edbcbf123c219009f6172bb
nozwock <57829219+nozwock@users.noreply.github.com>
2024-01-01 22:55:22 +0530

    Refactor save mod

--- crates/ddda_save_forge/src/save.rs
+++ crates/ddda_save_forge/src/save.rs
@@ -1,5 +1,24 @@
 // TODO: Equipped skills, Learned Skills, Inclinations
 
+// `class name mPlayerDataManual`: Non-checkpoint Save
+/// `class type sSave::playerData`
+pub struct SaveData {
+    pub pl_cmc_edit_and_param: PlayerPawnEditAndParam,
+    pub pl_game_data: PlayerGameData,
+}
+
+/// `class name PlCmcEditAndParam`
+pub struct PlayerPawnEditAndParam {
+    pub player: SaveDataPlayer,   // class name mPl
+    pub pawns: [SaveDataPawn; 3], // array name mCmc
+    pub item: [SaveDataItem; 4],  // array name mItem
+} // class type sSave::playerData > class name PlCmcEditAndParam
+
+/// `class name mPlGameData`
+pub struct PlayerGameData {
+    pub storage: SaveDataStorageItem,
+} // class type sSave::playerData > class name mPlGameData
+
 /// `class type cSAVE_DATA_PL`
 pub struct SaveDataPlayer {
     pub param: SaveDataParam,
@@ -10,29 +29,19 @@ pub struct SaveDataPawn {
     pub param: SaveDataParam,
 } // class name PlCmcEditAndParam > class name mCmc > class type cSAVE_DATA_CMC
 
-/// `class name mParam`\
-/// Player/Pawn parameters
-pub struct SaveDataParam {
-    pub gold: i32,                       // s32 name mGold
-    pub rift_crystals: i32,              // s32 name OnlinePoint
-    pub level: u8,                       // u8 name mLevel
-    pub exp: u32,                        // u32 name mExp
-    pub next_exp: u32,                   // u32 name mNextExp
-    pub hp: f32,                         // f32 name mHp
-    pub max_hp: f32,                     // f32 name mHpMax
-    pub max_hp_white: f32,               // f32 name mHpMaxWhite
-    pub stamina: f32,                    // f32 name mStamina
-    pub stamina_base: f32,               // f32 name mStaminaBase
-    pub stamina_from_lv: f32,            // f32 name mStaminaLv
-    pub base_strength: f32,              // f32 name mBasicAttack
-    pub base_defense: f32,               // f32 name mBasicDefend
-    pub base_magick: f32,                // f32 name mBasicMgcAttack
-    pub base_magick_defense: f32,        // f32 name mBasicMgcDefend
-    pub current_vocation: u8,            // u8 name mJob
-    pub vocation_level: [u8; 10],        // array name mJobLevel
-    pub discipline_point: [i32; 10],     // array name mJobPoint
-    pub equip_item: [ItemParamData; 12], // array name mEquipItem
-} // class name PlCmcEditAndParam > * > class name mParam
+/// `class type cSAVE_DATA_ITEM`\
+/// Player/Pawn Inventory Items
+pub struct SaveDataItem {
+    pub item_count: u32, // u32 name mItemCount
+    pub item_capacity: u32,
+    pub item: Vec<ItemParamData>, // array name mItem
+}
+
+pub struct SaveDataStorageItem {
+    pub item_count: u32, // u32 name mStorageItemCount
+    pub storage_item_capacity: u32,
+    pub storage_item: Vec<ItemParamData>, // array name mStorageItem
+} // Transparent container
 
 pub enum ItemQualityFlag {
     Raw = 3,
@@ -58,28 +67,26 @@ pub struct ItemParamData {
     pub key: u32,          // u32 name data.mKey
 }
 
-/// `class type cSAVE_DATA_ITEM`\
-/// Player/Pawn Inventory Items
-pub struct SaveDataItem {
-    pub item_count: u32, // u32 name mItemCount
-    pub item_capacity: u32,
-    pub item: Vec<ItemParamData>, // array name mItem
-}
-
-pub struct SaveDataStorageItem {
-    pub item_count: u32, // u32 name mStorageItemCount
-    pub storage_item_capacity: u32,
-    pub storage_item: Vec<ItemParamData>, // array name mStorageItem
-} // Transparent container
-
-/// `class name PlCmcEditAndParam`
-pub struct PlayerPawnEditAndParam {
-    pub player: SaveDataPlayer,   // class name mPl
-    pub pawns: [SaveDataPawn; 3], // array name mCmc
-    pub item: [SaveDataItem; 4],  // array name mItem
-} // class type sSave::playerData > class name PlCmcEditAndParam
-
-/// `class name mPlGameData`
-pub struct PlayerGameData {
-    pub storage: SaveDataStorageItem,
-} // class type sSave::playerData > class name mPlGameData
+/// `class name mParam`\
+/// Player/Pawn parameters
+pub struct SaveDataParam {
+    pub gold: i32,                       // s32 name mGold
+    pub rift_crystals: i32,              // s32 name OnlinePoint
+    pub level: u8,                       // u8 name mLevel
+    pub exp: u32,                        // u32 name mExp
+    pub next_exp: u32,                   // u32 name mNextExp
+    pub hp: f32,                         // f32 name mHp
+    pub max_hp: f32,                     // f32 name mHpMax
+    pub max_hp_white: f32,               // f32 name mHpMaxWhite
+    pub stamina: f32,                    // f32 name mStamina
+    pub stamina_base: f32,               // f32 name mStaminaBase
+    pub stamina_from_lv: f32,            // f32 name mStaminaLv
+    pub base_strength: f32,              // f32 name mBasicAttack
+    pub base_defense: f32,               // f32 name mBasicDefend
+    pub base_magick: f32,                // f32 name mBasicMgcAttack
+    pub base_magick_defense: f32,        // f32 name mBasicMgcDefend
+    pub current_vocation: u8,            // u8 name mJob
+    pub vocation_level: [u8; 10],        // array name mJobLevel
+    pub discipline_point: [i32; 10],     // array name mJobPoint
+    pub equip_item: [ItemParamData; 12], // array name mEquipItem
+} // class name PlCmcEditAndParam > * > class name mParam
